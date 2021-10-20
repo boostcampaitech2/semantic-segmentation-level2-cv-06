98bafad771dd7cad52bb368fdf4b3256f6248f10
JiyouSeo <seojiyu1113@gmail.com>
2021-10-20 09:31:28 +0000

    [FEAT] add setting color in utils.py, fix color csv file

--- utils.py
+++ utils.py
@@ -3,7 +3,9 @@
 import random
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
 
+class_colormap = pd.read_csv("./EDA/class_dict.csv")
 
 def _fast_hist(label_true, label_pred, n_class):
     mask = (label_true >= 0) & (label_true < n_class)
@@ -64,18 +66,52 @@ def grid_image(images, masks, preds, n=4, shuffle=False):
         ax[idx*3].grid(False)
 
         ax[idx*3+1] = figure.add_subplot(gs[idx, 1])
-        ax[idx*3+1].imshow(mask)
+        ax[idx*3+1].imshow(label_to_color_image(mask))
         ax[idx*3+1].grid(False)
 
         ax[idx*3+2] = figure.add_subplot(gs[idx, 2])
-        ax[idx*3+2].imshow(pred)
+        ax[idx*3+2].imshow(label_to_color_image(pred))
         ax[idx*3+2].grid(False)
         
     return figure
 
+def create_trash_label_colormap():
+    """Creates a label colormap used in Trash segmentation.
+    Returns:
+        A colormap for visualizing segmentation results.
+    """
+    global class_colormap
 
+    colormap = np.zeros((11, 3), dtype=np.uint8)
+    for inex, (_, r, g, b) in enumerate(class_colormap.values):
+        colormap[inex] = [r, g, b]
+    
+    return colormap
 
+def label_to_color_image(label):
+    """Adds color defined by the dataset colormap to the label.
 
+    Args:
+        label: A 2D array with integer type, storing the segmentation label.
+
+    Returns:
+        result: A 2D array with floating type. The element of the array
+                is the color indexed by the corresponding element in the input label
+                to the trash color map.
+
+    Raises:
+        ValueError: If label is not of rank 2 or its value is larger than color
+              map maximum entry.
+    """
+    if label.ndim != 2:
+        raise ValueError('Expect 2-D input label')
+
+    colormap = create_trash_label_colormap()
+
+    if np.max(label) >= len(colormap):
+        raise ValueError('label value too large.')
+
+    return colormap[label]
 
 # def label_accuracy_score(label_trues, label_preds, n_class):
 #     """Returns accuracy score evaluation result.
