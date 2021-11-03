efba2ee21c6c8661498e561c92c7278b8027463f
JKJ <ppskj178@gmail.com>
2021-11-03 09:32:18 +0000

    [FEAT} add utils for HRNET_MS

---
+++ one_off/makelist.py
@@ -0,0 +1,28 @@
+import os
+from pycocotools.coco import COCO
+import numpy as np
+import json
+
+def cocotolst(dataDir='../input/data', filename='test'):
+    annPath = '%s/%s.json' % (dataDir, filename)
+
+    with open(annPath, 'r') as f:
+        coco = json.load(f)
+    # coco = json.loads(annPath)
+    # imgIds = coco.getImgIds()
+
+    # print(type(coco["images"][0]))
+    # print(type(imgIds))
+
+    with open(filename+".lst", 'w') as f:
+        for img_info in coco["images"]:
+            # print(img_info)
+            img_file_name = img_info["file_name"]
+            img_path = img_file_name
+            mask_path = "pngmask/" + img_file_name[:-3] + "png"
+            f.write(img_path + " " + mask_path + "\n")
+            # f.write(img_path + "\n")
+   
+
+if __name__ == "__main__":
+    cocotolst()
