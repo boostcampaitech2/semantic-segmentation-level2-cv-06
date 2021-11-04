8426a4fc3cc2849f6a357ca239a0db409b51139b
jiwoo <ann9902@gmail.com>
2021-11-04 05:06:50 +0000

    [CHORE] for inferece

--- inference.py
+++ inference.py
@@ -28,7 +28,7 @@ def inference(model_dir, args):
         collate_fn=collate_fn
     )
 
-    model_module = getattr(import_module("model"), args.model)
+    model_module = getattr(import_module("models.model"), args.model)
     model = model_module(
         num_classes=11, pretrained=True
     )
@@ -36,49 +36,51 @@ def inference(model_dir, args):
     model_path = os.path.join(model_dir, 'best.pt')
     checkpoint = torch.load(model_path, map_location=device)
     state_dict = checkpoint.state_dict()
-    model.load_state_dict(state_dict)
-    model = model.to(device)
+    # model.load_state_dict(state_dict)
+    # model = model.to(device)
+    model = checkpoint.to(device)
+    # print(state_dict.keys())
 
-    size = 256
-    transform = A.Compose([A.Resize(size, size)])
+    # size = 256
+    # transform = A.Compose([A.Resize(size, size)])
 
-    model.eval()
+    # model.eval()
 
-    file_name_list = []
-    preds_array = np.empty((0, size*size), dtype=np.compat.long)
+    # file_name_list = []
+    # preds_array = np.empty((0, size*size), dtype=np.compat.long)
 
-    with torch.no_grad():
-        for step, (imgs, image_infos) in enumerate(tqdm(test_loader)):
+    # with torch.no_grad():
+    #     for step, (imgs, image_infos) in enumerate(tqdm(test_loader)):
 
-            # inference (512 x 512)
-            if args.model in ('FCNRes50', 'FCNRes101', 'DeepLabV3_Res50', 'DeepLabV3_Res101'):
-                outs = model(torch.stack(imgs).to(device))['out']
-                oms = torch.argmax(outs.squeeze(), dim=1).detach().cpu().numpy()
-            elif args.model in ('MscaleOCRNet'):
-                outs = model(torch.stack(imgs).to(device))
-                oms = torch.argmax(outs['pred'].squeeze(), dim=1).detach().cpu().numpy()
-            else:
-                outs = model(torch.stack(imgs).to(device))
-                oms = torch.argmax(outs.squeeze(), dim=1).detach().cpu().numpy()
+    #         # inference (512 x 512)
+    #         if args.model in ('FCNRes50', 'FCNRes101', 'DeepLabV3_Res50', 'DeepLabV3_Res101'):
+    #             outs = model(torch.stack(imgs).to(device))['out']
+    #             oms = torch.argmax(outs.squeeze(), dim=1).detach().cpu().numpy()
+    #         elif args.model in ('MscaleOCRNet'):
+    #             outs = model(torch.stack(imgs).to(device))
+    #             oms = torch.argmax(outs['pred'].squeeze(), dim=1).detach().cpu().numpy()
+    #         else:
+    #             outs = model(torch.stack(imgs).to(device))
+    #             oms = torch.argmax(outs.squeeze(), dim=1).detach().cpu().numpy()
             
             
-            # resize (256 x 256)
-            temp_mask = []
-            for img, mask in zip(np.stack(imgs), oms):
-                transformed = transform(image=img, mask=mask)
-                mask = transformed['mask']
-                temp_mask.append(mask)
+    #         # resize (256 x 256)
+    #         temp_mask = []
+    #         for img, mask in zip(np.stack(imgs), oms):
+    #             transformed = transform(image=img, mask=mask)
+    #             mask = transformed['mask']
+    #             temp_mask.append(mask)
 
-            oms = np.array(temp_mask)
-            oms = oms.reshape([oms.shape[0], size*size]).astype(int)
-            preds_array = np.vstack((preds_array, oms))
+    #         oms = np.array(temp_mask)
+    #         oms = oms.reshape([oms.shape[0], size*size]).astype(int)
+    #         preds_array = np.vstack((preds_array, oms))
 
-            file_name_list.append([i['file_name'] for i in image_infos])
+    #         file_name_list.append([i['file_name'] for i in image_infos])
     
-    print("End prediction!")
-    file_names = [y for x in file_name_list for y in x]
+    # print("End prediction!")
+    # file_names = [y for x in file_name_list for y in x]
 
-    return file_names, preds_array
+    # return file_names, preds_array
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -111,4 +113,4 @@ if __name__ == '__main__':
                                        ignore_index=True)
     
     # save submission.csv
-    submission.to_csv(output_dir+'/submission.csv', index=False)
+    submission.to_csv(output_dir+'/deeplab101_new_cp_submission.csv', index=False)
---
+++ model.py
@@ -0,0 +1,174 @@
+import torch
+import torch.nn as nn
+from torchvision import models
+import segmentation_models_pytorch as smp
+from models.hrnetv2 import HighResolutionNet
+from models.ocrnet import HRNet, HRNet_Mscale
+
+
+class FCNRes50(nn.Module):
+    def __init__(self, num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = models.segmentation.fcn_resnet50(pretrained=pretrained)
+        
+        # output class를 data set에 맞도록 수정
+        self.model.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class FCNRes101(nn.Module):
+    def __init__(self, num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = models.segmentation.fcn_resnet101(pretrained=pretrained)
+        
+        # output class를 data set에 맞도록 수정
+        self.model.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class DeepLabV3_Res50(nn.Module):
+    def __init__(self, num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = models.segmentation.deeplabv3_resnet50(pretrained=pretrained)
+        
+        # output class를 data set에 맞도록 수정
+        self.model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class DeepLabV3_Res101(nn.Module):
+    def __init__(self, num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = models.segmentation.deeplabv3_resnet101(pretrained=pretrained)
+        
+        # output class를 data set에 맞도록 수정
+        self.model.classifier[4] = nn.Conv2d(256, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, encoder='efficientnet-b0', weights='imagenet', num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = smp.Unet(
+            encoder_name=encoder,       # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=weights,    # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,              # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=num_classes,        # model output channels (number of classes in your dataset)
+            )
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class UNet_PlusPlus(nn.Module):
+    def __init__(self, encoder='efficientnet-b0', weights='imagenet', num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = smp.UnetPlusPlus(
+            encoder_name=encoder,       # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=weights,    # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,              # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=num_classes,        # model output channels (number of classes in your dataset)
+            )
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class FPN(nn.Module):
+    def __init__(self, encoder='efficientnet-b0', weights='imagenet', num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = smp.FPN(
+            encoder_name=encoder,       # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=weights,    # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,              # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=num_classes,        # model output channels (number of classes in your dataset)
+            )
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class PSPNet(nn.Module):
+    def __init__(self, encoder='efficientnet-b0', weights='imagenet', num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = smp.PSPNet(
+            encoder_name=encoder,       # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=weights,    # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,              # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=num_classes,        # model output channels (number of classes in your dataset)
+            )
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class DeepLabV3(nn.Module):
+    def __init__(self, encoder='efficientnet-b0', weights='imagenet', num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = smp.DeepLabV3(
+            encoder_name=encoder,       # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=weights,    # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,              # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=num_classes,        # model output channels (number of classes in your dataset)
+            )
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class DeepLabV3_Plus(nn.Module):
+    def __init__(self, encoder='efficientnet-b0', weights='imagenet', num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = smp.DeepLabV3Plus(
+            encoder_name=encoder,       # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+            encoder_weights=weights,    # use `imagenet` pre-trained weights for encoder initialization
+            in_channels=3,              # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+            classes=num_classes,        # model output channels (number of classes in your dataset)
+            )
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class OCRNet(nn.Module):
+    def __init__(self, num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = HRNet(num_classes=num_classes)
+    
+    def forward(self, x):
+        return self.model(x)
+
+
+class MscaleOCRNet(nn.Module):
+    def __init__(self, num_classes=11, pretrained=True):
+        super().__init__()
+        self.model = HRNet_Mscale(num_classes=num_classes)
+        if pretrained:
+            checkpoint = torch.load('/opt/ml/segmentation/semantic-segmentation-level2-cv-06/models/weights/cityscapes_ocrnet.HRNet_Mscale_outstanding-turtle.pth')
+            for k in list(checkpoint['state_dict'].keys()):
+                name = k.replace('module.', '')
+                checkpoint['state_dict'][name] = checkpoint['state_dict'].pop(k)
+            checkpoint = checkpoint['state_dict']
+            model_state_dict = self.model.state_dict()
+            
+            for k in model_state_dict.keys():
+                if k not in checkpoint:
+                    raise Exception("model state dict load key error")
+                elif model_state_dict[k].size() == checkpoint[k].size():
+                    model_state_dict[k] = checkpoint[k]
+                else:
+                    print(f"model state dict load skip {k}")
+
+            self.model.load_state_dict(model_state_dict)
+
+    def forward(self, x):
+        return self.model(x)
+
